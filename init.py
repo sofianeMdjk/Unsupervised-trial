c957919d5aa7b65e2ac18714237c623f632e6f75
sofiane.medjkoune <sofiane.medjkoune@protonmail.com>
2018-07-30 21:20:41 +0200

    basic fit

--- init.py
+++ init.py
@@ -1,1 +1,21 @@
-import sklearn 
+
+from sklearn import datasets
+
+from sklearn.cluster import KMeans
+
+# Loading dataset
+iris_df = datasets.load_iris()
+
+model = KMeans(n_clusters=3)
+
+model.fit(iris_df.data)
+
+
+predicted_label = model.predict([[7.2, 3.5, 0.8, 1.6]])
+
+# Prediction on the entire data
+all_predictions = model.predict(iris_df.data)
+
+# Printing Predictions
+print(predicted_label)
+print(all_predictions)
